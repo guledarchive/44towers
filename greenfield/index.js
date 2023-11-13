aa234a02c37b614a27ecc14c9914432f8c88f8bc
guledarchive <149301817+guledarchive@users.noreply.github.com>
2023-11-13 23:54:18 +1100

    Add files via upload

---
+++ greenfield/index.js
@@ -0,0 +1,7 @@
+var indexJSON = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"coordinates":[[
+    [lng: 144.74878273435155,lat: -37.824899976103175],
+    [lng: 144.7675530342982,lat: -37.826460638075055],
+    [lng: 144.76810873396647,lat: -37.81914474984005],
+    [lng: 144.74980151707933,lat: -37.8176327091827],
+    [lng: 144.74878273435155,lat: -37.824899976103175]
+]],"type":"Polygon"},"id":0}]}
